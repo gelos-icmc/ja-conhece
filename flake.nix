1be71874e4c7a51decec17b5f3b362e196fbbb06
Gabriel Fontes <hi@m7.rs>
2025-06-07 11:13:18 +0200

    use local nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-
   outputs = { self, nixpkgs }: let
     forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (s: f nixpkgs.legacyPackages.${s});
   in rec {
